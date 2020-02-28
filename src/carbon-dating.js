eb0f625e4e2ca00d7684c35c7a94ecddb973a3e0
Elena <helena_shevts@mail.ru>
2020-02-28 21:31:39 +0300

    fix: fix solution carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -2,12 +2,12 @@ const MODERN_ACTIVITY = 15;
 const HALF_LIFE_PERIOD = 5730;
 
 module.exports = function dateSample(sampleActivity) {
-  let number = +sampleActivity;
-  if (!sampleActivity || number >= 9000 || number <= 0 || typeof sampleActivity === 'object' || parseInt(sampleActivity) != sampleActivity ||
-    typeof sampleActivity === 'number' || number >= 15) {
-    return false;
-  }
-  let result = Math.log(MODERN_ACTIVITY / number) * HALF_LIFE_PERIOD / 0.693;
-
-  return Math.ceil(result);
+  let number = parseFloat(sampleActivity);
+    if (!sampleActivity || number >= 9000 || number <= 0 || typeof sampleActivity === 'object'
+    || number >= 15 || typeof sampleActivity === 'number' || Number.isNaN(number) === true) {
+      return false;
+    }
+    let result = Math.log(MODERN_ACTIVITY/number)*HALF_LIFE_PERIOD/0.693;
+    
+    return Math.ceil(result);
 };
