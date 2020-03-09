55b7cd8d9690182740b4143e1bae94e206392da0
Elena <helena_shevts@mail.ru>
2020-03-09 19:23:42 +0300

    fix: fix solution extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,10 +1,23 @@
 module.exports = function repeater(str, options) {
     for (key in options) {
         let array = [];
+        let number = 1;
+        if (options['repeatTimes'] === undefined) {
+            array.push(str);
+            array.push(options['addition']);
+            return array.join('');
+        } else if (options['separator'] === undefined) {
+            for (j = 1; j <= options['repeatTimes']; j++) {
+                if (j === options['repeatTimes']) {
+                    array.push(str.repeat(number));
+                } else {
+                    array.push(str.repeat(number) + '+');
+                }
+            }
+            return array.join('');
+        }
         for (let i = 1; i <= options['repeatTimes']; i++) {
-            let number = 1;
             if (i === options['repeatTimes']) {
-
                 array.push(str.repeat(number));
                 for (let j = 1; j <= options['additionRepeatTimes']; j++) {
                     if (j === options['additionRepeatTimes']) {
